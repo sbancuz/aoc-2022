b7a6b8cdf0412e60fee06a7377e9446521f2bd94
sbancuz <lucabancalelb@gmail.com>
2022-12-07 22:07:20 +0100

    Day6

---
+++ src/day6.rs
@@ -0,0 +1,55 @@
+use std::{vec, usize};
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+type InputType = String;
+type SolutionType = i32;
+
+#[aoc_generator(day6)]
+pub fn input_generator(input: &str) -> InputType { 
+    input.to_owned()
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(data: &InputType) -> SolutionType {
+    let mut buff: [char; 4] = ['-', '-', '-', '-'];
+    let mut pnt = 0;
+    let mut res = 0;
+    for c in data.chars() {
+        
+        buff[pnt] = c;
+        pnt = (pnt + 1) % 4;    
+        res += 1;
+        if !has_dup(&buff) && res > 3 {
+            break;
+        }
+    }
+    res
+}
+
+fn has_dup(arr: &[char]) -> bool {
+    for i in 0..arr.len() {
+        for j in (i + 1)..arr.len() {
+            if arr[i].eq(&arr[j]) {            
+                return true;
+            }
+        }   
+    }
+    false
+}
+#[aoc(day6, part2)]
+pub fn solve_part2(data: &InputType) -> SolutionType {
+    let mut buff: [char; 14] = ['-'; 14];
+    let mut pnt = 0;
+    let mut res = 0;
+    for c in data.chars() {
+        
+        buff[pnt] = c;
+        pnt = (pnt + 1) % 14;    
+        res += 1;
+        if !has_dup(&buff) && res > 13 {
+            break;
+        }
+    }
+    res
+}
